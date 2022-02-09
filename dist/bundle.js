49cb0921e26ddc32ede44a0fe1f699451d89040e
Kilu691 <kilu691@gmail.com>
2022-02-09 21:02:33 +0100

    Initial commit

---
+++ dist/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("elo")})();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiJtQkFBQUEsUUFBUUMsSUFBSSxRIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdGFzay8uL2FwcC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnZWxvJyk7Il0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwic291cmNlUm9vdCI6IiJ9
