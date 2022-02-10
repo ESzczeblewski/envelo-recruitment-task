1b4e73acd4617c6cc65400f50ba58d624fedf7f5
Kilu691 <kilu691@gmail.com>
2022-02-10 23:33:02 +0100

    add-final-build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './app.ts',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         publicPath: '/docs/',
     },
     devServer: {
